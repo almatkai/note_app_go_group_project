e7f526a265f0b9b579addc293404a7b9489e2148
almatkai <92521753+almatkai@users.noreply.github.com>
2022-11-08 18:28:39 +0600

    first commit

---
+++ lets-go-snippetbox-main/cmd/web/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"snippetbox.eegurt.net/internal/models"
+
+	"github.com/alexedwards/scs/pgxstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type application struct {
+	infoLog        *log.Logger
+	errorLog       *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
+}
+
+func main() {
+	addr := flag.String("addr", "localhost:4000", "HTTP network address") //
+	dsn := flag.String("dsn", "postgresql://youruser:1234@localhost:5432/go_lang", "Postgres data source name")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)                  //New Custom Logger INFO
+	errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile) //New Custom Logger ERROR
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+	templateCache, err := newTemplateCache()
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Store = pgxstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
+	app := &application{
+		infoLog:        infoLog,
+		errorLog:       errorLog,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
+	}
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.Connect(context.Background(), dsn)
+	//ctx := context.Background()
+	//Эта функция возвращает пустой контекст.
+	//Она должна использоваться только на высоком уровне
+	//(в main или обработчике запросов высшего уровня).
+	//Он может быть использован для получения других контекстов,
+	//которые мы обсудим позже.
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
